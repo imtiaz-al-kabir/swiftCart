e1a5d1f0ade19f849ecd25bf153145f019e26491
imtiaz-al-kabir <imtiazalkabir@gmail.com>
2026-03-15 23:15:50 +0600

    application folder strucyure created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "../components/ui/button";
+
 const page = () => {
   return (
     <div>
-      <h1 className="text-3xl f">hello</h1>
+      <h1 className="text-3xl f ">hello</h1>
+      <Button className="bg-shop_orange">click me</Button>
     </div>
   );
 };
